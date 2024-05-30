f912bf035a4124a0ba7af9185935245617b8007a
bechong <bechong@paloaltonetworks.com>
2024-05-30 15:37:33 +0800

    revised file

--- gcp/resource-count-gcp.sh
+++ gcp/resource-count-gcp.sh
@@ -28,9 +28,15 @@
 #
 # - gcloud projects list
 # - gcloud compute instances list
-# - gcloud compute routers list
-# - gcloud compute routers nats list
 # - gcloud sql instances list
+# - gcloud storage ls 
+# - gcloud filestore instances list
+# - gcloud alpha bq datasets list
+# - gcloud bigtable instances list 
+# - gcloud spanner instances list
+# - gcloud redis instances list
+# - gcloud memcache instances list
+# - gcloud firestore databases list
 ##########################################################################################
 
 ##########################################################################################
@@ -76,30 +82,6 @@ gcloud_compute_instances_list() {
   fi
 }
 
-gcloud_compute_routers_list() {
-  # shellcheck disable=SC2086
-  RESULT=$(gcloud compute routers list --project "${1}" --format json $VERBOSITY_ARGS 2>/dev/null)
-  if [ $? -eq 0 ]; then
-    echo "${RESULT}"
-  fi
-}
-
-gcloud_compute_routers_nats_list() {
-  # shellcheck disable=SC2086
-  RESULT=$(gcloud compute routers nats list --project "${1}" --region "${2}" --router "${3}" --format json $VERBOSITY_ARGS 2>/dev/null)
-  if [ $? -eq 0 ]; then
-    echo "${RESULT}"
-  fi
-}
-
-gcloud_compute_backend_services_list() {
-  # shellcheck disable=SC2086
-  RESULT=$(gcloud compute backend-services list --project "${1}" --format json $VERBOSITY_ARGS 2>/dev/null)
-  if [ $? -eq 0 ]; then
-    echo "${RESULT}"
-  fi
-}
-
 gcloud_sql_instances_list() {
   # shellcheck disable=SC2086
   RESULT=$(gcloud sql instances list --project "${1}" --format json $VERBOSITY_ARGS 2>/dev/null)
@@ -108,6 +90,69 @@ gcloud_sql_instances_list() {
   fi
 }
 
+gcloud_storage_ls() {
+  # shellcheck disable=SC2086
+  RESULT=$(gcloud storage ls --project "${1}" --format json $VERBOSITY_ARGS 2>/dev/null)
+  if [ $? -eq 0 ]; then
+    echo "${RESULT}"
+  fi
+}
+
+gcloud_filestore_instance_list() {
+  # shellcheck disable=SC2086
+  RESULT=$(gcloud filestore instances list --project "${1}" --format json $VERBOSITY_ARGS 2>/dev/null)
+  if [ $? -eq 0 ]; then
+    echo "${RESULT}"
+  fi
+}
+
+gcloud_alpha_bq_datasets_list() {
+  # shellcheck disable=SC2086
+  RESULT=$(gcloud alpha bq datasets list --project "${1}" --format json $VERBOSITY_ARGS 2>/dev/null)
+  if [ $? -eq 0 ]; then
+    echo "${RESULT}"
+  fi
+}
+
+gcloud_bigtable_instances_list() {
+  # shellcheck disable=SC2086
+  RESULT=$(gcloud bigtable instances list --project "${1}" --format json $VERBOSITY_ARGS 2>/dev/null)
+  if [ $? -eq 0 ]; then
+    echo "${RESULT}"
+  fi
+}
+
+gcloud_spanner_instances_list() {
+  # shellcheck disable=SC2086
+  RESULT=$(gcloud spanner instances list --project "${1}" --format json $VERBOSITY_ARGS 2>/dev/null)
+  if [ $? -eq 0 ]; then
+    echo "${RESULT}"
+  fi
+}
+
+gcloud_redis_instances_list() {
+  # shellcheck disable=SC2086
+  RESULT=$(gcloud redis instances list --project "${1}" --format json $VERBOSITY_ARGS 2>/dev/null)
+  if [ $? -eq 0 ]; then
+    echo "${RESULT}"
+  fi
+}
+
+gcloud_memcache_instances_list() {
+  # shellcheck disable=SC2086
+  RESULT=$(gcloud memcache instances list --project "${1}" --format json $VERBOSITY_ARGS 2>/dev/null)
+  if [ $? -eq 0 ]; then
+    echo "${RESULT}"
+  fi
+}
+
+gcloud_firestore_databases_list() {
+  # shellcheck disable=SC2086
+  RESULT=$(gcloud firestore databases list --project "${1}" --format json $VERBOSITY_ARGS 2>/dev/null)
+  if [ $? -eq 0 ]; then
+    echo "${RESULT}"
+  fi
+}
 ####
 
 get_project_list() {
@@ -121,18 +166,30 @@ get_project_list() {
 
 reset_project_counters() {
   COMPUTE_INSTANCES_COUNT=0
-  COMPUTE_NAT_COUNT=0
-  COMPUTE_BACKEND_SERVICES_COUNT=0
   SQL_INSTANCES_COUNT=0
   WORKLOAD_COUNT=0
+  STORAGE_COUNT=0
+  FILESTORE_COUNT=0
+  BIGQUERY_COUNT=0
+  BIGTABLE_COUNT=0
+  SPANNER_COUNT=0
+  REDIS_COUNT=0
+  MEMCACHE_COUNT=0
+  FIRESTORE_COUNT=0
 }
 
 reset_global_counters() {
   COMPUTE_INSTANCES_COUNT_GLOBAL=0
-  COMPUTE_NAT_COUNT_GLOBAL=0
-  COMPUTE_BACKEND_SERVICES_COUNT_GLOBAL=0
   SQL_INSTANCES_COUNT_GLOBAL=0
   WORKLOAD_COUNT_GLOBAL=0
+  STORAGE_COUNT_GLOBAL=0
+  FILESTORE_COUNT_GLOBAL=0
+  BIGQUERY_COUNT_GLOBAL=0
+  BIGTABLE_COUNT_GLOBAL=0
+  SPANNER_COUNT_GLOBAL=0
+  REDIS_COUNT_GLOBAL=0
+  MEMCACHE_COUNT_GLOBAL=0
+  FIRESTORE_COUNT_GLOBAL=0
 }
 
 ##########################################################################################
@@ -151,33 +208,57 @@ count_project_resources() {
     COMPUTE_INSTANCES_COUNT=$((COMPUTE_INSTANCES_COUNT + RESOURCE_COUNT))
     echo "  Count of Running Compute Instances: ${COMPUTE_INSTANCES_COUNT}"
 
-    ROUTERS=($(gcloud_compute_routers_list "${PROJECT}" | jq  -r '.[] | "\(.name);\(.region)"'))
-    for ROUTER in "${ROUTERS[@]}"
-    do
-        ROUTER_REGION=${ROUTER##*/}
-        ROUTER_NAME=$(cut -d ';' -f 1 <<< "${ROUTER}")
-        RESOURCE_COUNT=$(gcloud_compute_routers_nats_list "${PROJECT}" "${ROUTER_REGION}" "${ROUTER_NAME}" | jq -r '.[].name' | wc -l)
-        COMPUTE_NAT_COUNT=$((COMPUTE_NAT_COUNT + RESOURCE_COUNT))
-    done
-    echo "  Count of NAT: ${COMPUTE_NAT_COUNT}"
-
-    RESOURCE_COUNT=$(gcloud_compute_backend_services_list "${PROJECT}" | jq '.[].name' | wc -l)
-    COMPUTE_BACKEND_SERVICES_COUNT=$((COMPUTE_BACKEND_SERVICES_COUNT + RESOURCE_COUNT))
-    echo "  Count of Compute Load Balancing Services: ${COMPUTE_BACKEND_SERVICES_COUNT}"
-
     RESOURCE_COUNT=$(gcloud_sql_instances_list "${PROJECT}" | jq '.[].name' | wc -l)
     SQL_INSTANCES_COUNT=$((SQL_INSTANCES_COUNT + RESOURCE_COUNT))
     echo "  Count of SQL Instances: ${SQL_INSTANCES_COUNT}"
 
-    WORKLOAD_COUNT=$((COMPUTE_INSTANCES_COUNT + COMPUTE_NAT_COUNT + COMPUTE_BACKEND_SERVICES_COUNT + SQL_INSTANCES_COUNT))
+    RESOURCE_COUNT=$(gcloud_storage_ls "${PROJECT}" | jq '.[].name' | wc -l)
+    STORAGE_COUNT=$((STORAGE_COUNT + RESOURCE_COUNT))
+    echo "  Count of Storage Buckets: ${STORAGE_COUNT}"
+
+    RESOURCE_COUNT=$(gcloud_filestore_instance_list "${PROJECT}" | jq '.[].name' | wc -l)
+    FILESTORE_COUNT=$((FILESTORE_COUNT + RESOURCE_COUNT))
+    echo "  Count of Filestore: ${FILESTORE_COUNT}"
+
+    RESOURCE_COUNT=$(gcloud_alpha_bq_datasets_list "${PROJECT}" | jq '.[].name' | wc -l)
+    BIGQUERY_COUNT=$((BIGQUERY_COUNT + RESOURCE_COUNT))
+    echo "  Count of BigQuery: ${BIGQUERY_COUNT}"
+
+    RESOURCE_COUNT=$(gcloud_bigtable_instances_list "${PROJECT}" | jq '.[].name' | wc -l)
+    BIGTABLE_COUNT=$((BIGTABLE_COUNT + RESOURCE_COUNT))
+    echo "  Count of BigTable: ${BIGTABLE_COUNT}"
+
+    RESOURCE_COUNT=$(gcloud_spanner_instances_list "${PROJECT}" | jq '.[].name' | wc -l)
+    SPANNER_COUNT=$((SPANNER_COUNT + RESOURCE_COUNT))
+    echo "  Count of Spanner: ${SPANNER_COUNT}"
+
+    RESOURCE_COUNT=$(gcloud_redis_instances_list "${PROJECT}" | jq '.[].name' | wc -l)
+    REDIS_COUNT=$((REDIS_COUNT + RESOURCE_COUNT))
+    echo "  Count of Redis: ${REDIS_COUNT}"
+
+    RESOURCE_COUNT=$(gcloud_memcache_instances_list "${PROJECT}" | jq '.[].name' | wc -l)
+    MEMCACHE_COUNT=$((MEMCACHE_COUNT + RESOURCE_COUNT))
+    echo "  Count of Memcache: ${MEMCACHE_COUNT}"
+
+    RESOURCE_COUNT=$(gcloud_firestore_databases_list "${PROJECT}" | jq '.[].name' | wc -l)
+    FIRESTORE_COUNT=$((FIRESTORE_COUNT + RESOURCE_COUNT))
+    echo "  Count of Firestore: ${FIRESTORE_COUNT}"
+
+    WORKLOAD_COUNT=$((COMPUTE_INSTANCES_COUNT + SQL_INSTANCES_COUNT + STORAGE_COUNT + FILESTORE_COUNT + BIGQUERY_COUNT + BIGTABLE_COUNT + SPANNER_COUNT + REDIS_COUNT + MEMCACHE_COUNT + FIRESTORE_COUNT))
     echo "Total billable resources for Project ${PROJECTS[$PROJECT_INDEX]}: ${WORKLOAD_COUNT}"
     echo "###################################################################################"
     echo ""
 
     COMPUTE_INSTANCES_COUNT_GLOBAL=$((COMPUTE_INSTANCES_COUNT_GLOBAL + COMPUTE_INSTANCES_COUNT))
-    COMPUTE_NAT_COUNT_GLOBAL=$((COMPUTE_NAT_COUNT_GLOBAL + COMPUTE_NAT_COUNT))
-    COMPUTE_BACKEND_SERVICES_COUNT_GLOBAL=$((COMPUTE_BACKEND_SERVICES_COUNT_GLOBAL + COMPUTE_BACKEND_SERVICES_COUNT))
     SQL_INSTANCES_COUNT_GLOBAL=$((SQL_INSTANCES_COUNT_GLOBAL + SQL_INSTANCES_COUNT))
+    STORAGE_COUNT_GLOBAL=$((STORAGE_COUNT_GLOBAL + STORAGE_COUNT))
+    FILESTORE_COUNT_GLOBAL=$((FILESTORE_COUNT_GLOBAL + FILESTORE_COUNT))
+    BIGQUERY_COUNT_GLOBAL=$((BIGQUERY_COUNT_GLOBAL + BIGQUERY_COUNT))
+    BIGTABLE_COUNT_GLOBAL=$((BIGTABLE_COUNT_GLOBAL + BIGTABLE_COUNT))
+    SPANNER_COUNT_GLOBAL=$((SPANNER_COUNT_GLOBAL + SPANNER_COUNT))
+    REDIS_COUNT_GLOBAL=$((REDIS_COUNT_GLOBAL + REDIS_COUNT))
+    MEMCACHE_COUNT_GLOBAL=$((MEMCACHE_COUNT_GLOBAL + MEMCACHE_COUNT))
+    FIRESTORE_COUNT_GLOBAL=$((FIRESTORE_COUNT_GLOBAL + FIRESTORE_COUNT))
 
     reset_project_counters
   done
@@ -185,10 +266,17 @@ count_project_resources() {
   echo "###################################################################################"
   echo "Totals for all projects"
   echo "  Count of Running Compute Instances: ${COMPUTE_INSTANCES_COUNT_GLOBAL}"
-  echo "  Count of NAT: ${COMPUTE_NAT_COUNT_GLOBAL}"
-  echo "  Count of Compute Load Balancing Services: ${COMPUTE_BACKEND_SERVICES_COUNT_GLOBAL}"
   echo "  Count of SQL Instances: ${SQL_INSTANCES_COUNT_GLOBAL}"
-  WORKLOAD_COUNT_GLOBAL=$((COMPUTE_INSTANCES_COUNT_GLOBAL + COMPUTE_NAT_COUNT_GLOBAL + COMPUTE_BACKEND_SERVICES_COUNT_GLOBAL + SQL_INSTANCES_COUNT_GLOBAL))
+  echo "  Count of Storage Buckets: ${STORAGE_COUNT_GLOBAL}"
+  echo "  Count of Filestore: ${FILESTORE_COUNT_GLOBAL}"
+  echo "  Count of BigQuery: ${BIGQUERY_COUNT_GLOBAL}"
+  echo "  Count of BigTable: ${BIGTABLE_COUNT_GLOBAL}"
+  echo "  Count of Spanner: ${SPANNER_COUNT_GLOBAL}"
+  echo "  Count of Redis: ${REDIS_COUNT_GLOBAL}"
+  echo "  Count of Memcache: ${MEMCACHE_COUNT_GLOBAL}"
+  echo "  Count of Firestore: ${FIRESTORE_COUNT_GLOBAL}"
+
+  WORKLOAD_COUNT_GLOBAL=$((COMPUTE_INSTANCES_COUNT_GLOBAL + SQL_INSTANCES_COUNT_GLOBAL + STORAGE_COUNT_GLOBAL + FILESTORE_COUNT_GLOBAL + BIGQUERY_COUNT_GLOBAL + BIGTABLE_COUNT_GLOBAL + SPANNER_COUNT_GLOBAL + REDIS_COUNT_GLOBAL + MEMCACHE_COUNT_GLOBAL + FIRESTORE_COUNT_GLOBAL))
   echo "Total billable resources for all projects: ${WORKLOAD_COUNT_GLOBAL}"
   echo "###################################################################################"
 }
